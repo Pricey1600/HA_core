1de8d5b8252d2f072e7cadce720616cb8c3ca62b
PriceyPixel <jamesprice47@gmail.com>
2025-04-13 14:02:17 +0100

    Basic status sensor

---
+++ homeassistant/components/cleddau_bridge/const.py
@@ -0,0 +1,5 @@
+"""Constants for the Cleddau Bridge Status integration."""
+
+DOMAIN = "cleddau_bridge"
+
+DEFAULT_SCAN_INTERVAL = 10
---
+++ tests/components/cleddau_bridge/__init__.py
@@ -0,0 +1,1 @@
+"""Tests for the Cleddau Bridge Status integration."""
